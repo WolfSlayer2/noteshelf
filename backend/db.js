86bcbe13fd2324f50316b3acb79b9401809c836b
Divyansh Asthana <shivammohan850@gmail.com>
2023-09-11 22:23:40 +0530

    mongodb connection

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/"
+
+//run mongodb server in services to avoid selection error
+const connectToMongo = async () =>{
+   await mongoose.connect(mongoURI).then(()=> console.log("Connected Successfully")).catch(err => console.log(err));
+}
+
+module.exports = connectToMongo;
