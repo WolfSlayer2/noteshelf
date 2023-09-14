5809908f4a004be13db105757f3e5c3567f94307
Divyansh Asthana <shivammohan850@gmail.com>
2023-09-15 02:52:33 +0530

    Minor changes & thunderclient collections

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,24 +3,35 @@ const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
 
-//Create a User using: POST "/api/auth/" Doesn't require Auth
-router.post('/',[
-    body('name','Enter a valid name').isLength({ min: 3 }),
-    body('email','Enter a valid email').isEmail(),
-    body('password','Pass. length should be atleast 5 characters').isLength({ min: 5 }),
-] , (req, res) => {
-    const errors = validationResult(req);
+//Create a User using: POST "/api/auth/createuser" No Login required
+router.post('/createuser', [
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Pass. length should be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+  //return Bad request & errors if found
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      }).then(user => res.json(user))
-      .catch(err => {console.log(err) 
-      res.json({error: "Enter unique email value", message: err.message})})
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  //check whether user with this email exists already
+  try {
+  let user = await User.findOne({ email: req.body.email });
+  if(user){
+    return res.status(400).json({error: 'Sorry, User with this email already exits'})
+  }
+  user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  })
+
+  res.json(user)
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some error occured");
+}
 })
 
 module.exports = router
