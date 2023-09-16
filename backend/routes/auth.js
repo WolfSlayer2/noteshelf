36f4f6b4bf18fa69ed2b89dd42005eff81100847
Divyansh Asthana <shivammohan850@gmail.com>
2023-09-17 01:43:45 +0530

    added authentication using jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,11 @@ const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+//secret code
+require('dotenv').config()
+const secret_key = process.env.JWT_SECRET
 
 //Create a User using: POST "/api/auth/createuser" No Login required
 router.post('/createuser', [
@@ -22,7 +27,7 @@ router.post('/createuser', [
     if (user) {
       return res.status(400).json({ error: 'Sorry, User with this email already exits' })
     }
-    //using bcrypt
+    //using bcrypt to add salt
     const salt = await bcrypt.genSalt(10);
     const securePass = await bcrypt.hash(req.body.password,salt);
     user = await User.create({
@@ -30,8 +35,16 @@ router.post('/createuser', [
       email: req.body.email,
       password: securePass,
     })
+    
+    //using JWT for authentication
+    const data = {
+        user:{ 
+          id: user.id
+        }
+    }
+    const authtoken = jwt.sign(data,secret_key);
+    res.json({authtoken})
 
-    res.json(user)
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Some error occured");
