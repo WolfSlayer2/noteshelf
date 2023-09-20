f3575d9e7182b3f5c5d5c98e1b3c3cfbc190da7b
Divyansh Asthana <shivammohan850@gmail.com>
2023-09-21 01:13:20 +0530

    Update Note end point added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -16,20 +16,20 @@ router.get('/fetchnotes', fetchuser, async (req, res) => {
 })
 
 //Route 2: Add a new note using: POST "/api/notes/addnote" Login required
-router.get('/addnote', fetchuser, [
+router.post('/addnote', fetchuser, [
   body('title', 'Enter a valid title').isLength({ min: 3 }),
   body('description', 'Description should be atleast 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
-  
+
   try {
-    const {title, description, tag} = req.body;
+    const { title, description, tag } = req.body;
     //return Bad request & errors if found in validation
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     const note = new Notes({
-        title, description, tag, user: req.user.id
+      title, description, tag, user: req.user.id
     })
     const savedNote = await note.save()
     res.json(savedNote)
@@ -39,4 +39,34 @@ router.get('/addnote', fetchuser, [
   }
 })
 
+//Route 3: Update an existing note using: PUT "/api/notes/updatenote" Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  //Create a new note
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+
+  //Find the note and update it with new note
+  let note = await Notes.findById(req.params.id);
+  //if note not exists
+  if(!note){
+    return res.status(404).send("Not Found")
+  }
+
+  //user check
+  if(note.user.toString() !== req.user.id){
+    return res.status(401).send("Access Denied")
+  }
+
+  note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+  res.json(note);
+})
 module.exports = router
